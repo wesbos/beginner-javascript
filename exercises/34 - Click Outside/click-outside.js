4469f535f28e2bb5de3d34730cd80b00ddfb0e10
Wes Bos <wesbos@gmail.com>
2019-09-30 15:38:46 -0400

    Scroll to accept

---
+++ exercises/34 - Click Outside/click-outside.js
@@ -0,0 +1,44 @@
+const cardButtons = document.querySelectorAll('.card button');
+const modalOuter = document.querySelector('.modal-outer');
+const modalInner = document.querySelector('.modal-inner');
+
+function handleCardButtonClick(event) {
+  const button = event.currentTarget;
+  const card = button.closest('.card');
+  // Grab the image src
+  const imgSrc = card.querySelector('img').src;
+  const desc = card.dataset.description;
+  const name = card.querySelector('h2').textContent;
+  // populate the modal with the new info
+  modalInner.innerHTML = `
+    <img width="600" height="600" src="${imgSrc.replace(
+      '200',
+      '600'
+    )}" alt="${name}"/>
+    <p>${desc}</p>
+  `;
+  // show the modal
+  modalOuter.classList.add('open');
+}
+
+cardButtons.forEach(button =>
+  button.addEventListener('click', handleCardButtonClick)
+);
+
+function closeModal() {
+  modalOuter.classList.remove('open');
+}
+
+modalOuter.addEventListener('click', function(event) {
+  const isOutside = !event.target.closest('.modal-inner');
+  if (isOutside) {
+    closeModal();
+  }
+});
+
+window.addEventListener('keydown', event => {
+  console.log(event);
+  if (event.key === 'Escape') {
+    closeModal();
+  }
+});
---
+++ exercises/35 - Scroll To Accept/scroll-to-accept-FINISHED.js
@@ -0,0 +1,17 @@
+const terms = document.querySelector('.terms-and-conditions');
+const watch = document.querySelector('.watch');
+const button = document.querySelector('.accept');
+
+function obCallback(payload) {
+  if (payload[0].intersectionRatio === 1) {
+    button.disabled = false;
+    // stop observing the button
+    ob.unobserve(terms.lastElementChild);
+  }
+}
+const ob = new IntersectionObserver(obCallback, {
+  root: terms,
+  threshold: 1,
+});
+
+ob.observe(terms.lastElementChild);
