4e4cd0b25f1d1ade6ef4948394ff4904350e046a
A-J Roos <asjas@outlook.com>
2020-04-29 17:37:04 +0200

    create starter and finished files for video 78

---
+++ playground/modules/handlers-FINISHED.js
@@ -0,0 +1,4 @@
+export async function handleButtonClick(event) {
+  const { localCurrency, default: currency } = await import("./currencies.js");
+  console.log(localCurrency, currency);
+}
--- playground/modules/handlers.js
+++
@@ -1,4 +0,0 @@
-export async function handleButtonClick(event) {
-  const { localCurrency, default: currency } = await import('./currencies.js');
-  console.log(localCurrency, currency);
-}
