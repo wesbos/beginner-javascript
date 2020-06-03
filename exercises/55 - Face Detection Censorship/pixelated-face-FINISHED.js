db0689709279f4a16e44245e5ee7a02cd21d0630
A-J Roos <asjas@outlook.com>
2020-06-03 19:38:49 +0200

    add a note about face detection not working on all systems

--- exercises/55 - Face Detection Censorship/pixelated-face-FINISHED.js
+++ exercises/55 - Face Detection Censorship/pixelated-face-FINISHED.js
@@ -1,8 +1,12 @@
-const video = document.querySelector('.webcam');
-const canvas = document.querySelector('.video');
-const ctx = canvas.getContext('2d');
-const faceCanvas = document.querySelector('.face');
-const faceCtx = faceCanvas.getContext('2d');
+// The face detection does not work on all browsers and operating systems.
+// If you are getting a `Face detection service unavailable` error or similar,
+// it's possible that it won't work for you at the moment.
+
+const video = document.querySelector(".webcam");
+const canvas = document.querySelector(".video");
+const ctx = canvas.getContext("2d");
+const faceCanvas = document.querySelector(".face");
+const faceCtx = faceCanvas.getContext("2d");
 const faceDetector = new window.FaceDetector();
 const optionsInputs = document.querySelectorAll(
   '.controls input[type="range"]'
@@ -17,7 +21,7 @@ function handleOption(event) {
   const { value, name } = event.currentTarget;
   options[name] = parseFloat(value);
 }
-optionsInputs.forEach(input => input.addEventListener('input', handleOption));
+optionsInputs.forEach((input) => input.addEventListener("input", handleOption));
 
 // Write a fucntion that will populate the users video
 async function populateVideo() {
@@ -45,7 +49,7 @@ async function detect() {
 function drawFace(face) {
   const { width, height, top, left } = face.boundingBox;
   ctx.clearRect(0, 0, canvas.width, canvas.height);
-  ctx.strokeStyle = '#ffc600';
+  ctx.strokeStyle = "#ffc600";
   ctx.lineWidth = 2;
   ctx.strokeRect(left, top, width, height);
 }
---
+++ exercises/55 - Face Detection Censorship/pixelated-face.js
@@ -0,0 +1,3 @@
+// The face detection does not work on all browsers and operating systems.
+// If you are getting a `Face detection service unavailable` error or similar,
+// it's possible that it won't work for you at the moment.
