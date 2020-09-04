b712de4f25ac2f511df637de210428fbf6c9700c
Matthew Bolton <matthewwbolton@gmail.com>
2020-09-04 11:37:07 -0400

    completed all excercises in tabs

--- exercises/36 - Tabs/tabs.js
+++ exercises/36 - Tabs/tabs.js
@@ -1,1 +1,26 @@
-console.log('ya ya wes we get it.. IT WORKS!');
+console.log("ya ya wes we get it.. IT WORKS!");
+
+const tabs = document.querySelector(".tabs");
+const tabButtons = tabs.querySelectorAll('[role="tab"]');
+const tabPanels = Array.from(tabs.querySelectorAll('[role= "tabpanel"]'));
+
+function handleTabClick(e) {
+  tabPanels.forEach((panel) => (panel.hidden = true));
+  tabButtons.forEach((tabButton) =>
+    tabButton.setAttribute("aria-selected", false)
+  );
+  event.currentTarget.setAttribute("aria-selected", true);
+  const { id } = e.currentTarget;
+
+  //   Method 1:
+  // const tabPanel = tabs.querySelector('[aria-labelledby="${id}"]')
+
+  const tabPanel = tabPanels.find(
+    (panel) => panel.getAttribute("aria-labelledby") === id
+  );
+  tabPanel.hidden = false;
+}
+
+tabButtons.forEach((button) =>
+  button.addEventListener("click", handleTabClick)
+);
