8f46d708db791acd7be8adb101213950ad89d133
Matthew Bolton <matthewwbolton@gmail.com>
2020-09-04 10:49:30 -0400

    -completed terms and conditions excercises in scroll to accept

--- exercises/35 - Scroll To Accept/scroll-to-accept.js
+++ exercises/35 - Scroll To Accept/scroll-to-accept.js
@@ -1,1 +1,19 @@
-console.log('IT WORKS!');
+const terms = document.querySelector(".terms-and-conditions");
+const watch = document.querySelector(".watch");
+const button = document.querySelector(".accept");
+
+const ob = new IntersectionObserver(obCallback, { root: terms, threshold: 1 });
+
+function obCallback(payload) {
+  if (payload[0].intersectionRatio === 1) {
+    button.disabled = false;
+    ob.unobserve(terms.lastElementChild);
+  }
+}
+
+// terms.addEventListener("scroll", function(e) {
+//   console.log(e.currentTarget.scrollTop);
+//   console.log(e.currentTarget.scrollHeight);
+// });
+
+ob.observe(terms.lastElementChild);
