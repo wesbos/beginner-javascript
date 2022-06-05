fd8f5580e380bc2f51942a99b0f606f4162d3008
Adam Kearney <K3ARN3Y@users.noreply.github.com>
2022-06-05 07:15:44 -0700

    add documentation for demo server permissions

--- exercises/75 - CORS and Recipes/scripts.js
+++ exercises/75 - CORS and Recipes/scripts.js
@@ -1,2 +1,7 @@
-// The Recipe Puppy API used in the course is broken
-// Please use this replacement API URL "https://recipes.beginnerjavascript.com/api"
+/*
+1. The Recipe Puppy API used in the course is broken.
+- Please use this replacement API URL "https://recipes.beginnerjavascript.com/api"
+
+2. To use Cors Anywhere, you now need to request temporary access.
+- Please do that here: https://cors-anywhere.herokuapp.com/corsdemo
+*/
